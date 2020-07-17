7c4e5bfc269b254cb306428545596dfbbf3ace3b
Nikita Rogovoy <mammothmkiv@gmail.com>
2020-07-18 00:11:10 +0300

    Outlined base project structure

---
+++ README.md
@@ -0,0 +1,1 @@
+Cognitio Core Backend
---
+++ server-api/src/main/java/tech/cognitio/backend/server/CognitioApplication.java
@@ -0,0 +1,11 @@
+package tech.cognitio.backend.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CognitioApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(CognitioApplication.class, args);
+    }
+}
